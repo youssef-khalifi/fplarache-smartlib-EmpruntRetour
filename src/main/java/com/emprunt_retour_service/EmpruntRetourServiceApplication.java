d802bdcf9d480a1067c6d9891b0d214cb7c052b1
youssef-khalifi <youssefkhalifi767@gmail.com>
2024-11-07 19:34:37 +0100

    creation of the project

---
+++ src/main/java/com/emprunt_retour_service/EmpruntRetourServiceApplication.java
@@ -0,0 +1,13 @@
+package com.emprunt_retour_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpruntRetourServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmpruntRetourServiceApplication.class, args);
+    }
+
+}
