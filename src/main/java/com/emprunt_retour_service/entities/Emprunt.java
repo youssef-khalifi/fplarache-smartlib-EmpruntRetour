541fdc72575e45980a4b66d97734401395fcd473
youssef-khalifi <youssefkhalifi767@gmail.com>
2024-11-07 19:57:00 +0100

    add the Data Jpa annotaion

--- src/main/java/com/emprunt_retour_service/entities/Emprunt.java
+++ src/main/java/com/emprunt_retour_service/entities/Emprunt.java
@@ -2,12 +2,18 @@ package com.emprunt_retour_service.entities;
 
 import com.emprunt_retour_service.model.Livre;
 import com.emprunt_retour_service.model.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
-import java.util.Date;
 
+@Data@Entity@AllArgsConstructor@NoArgsConstructor@Builder
 public class Emprunt {
 
+    @Id@GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private LocalDate dateDebut;
     private LocalDate dateFin;
@@ -16,6 +22,8 @@ public class Emprunt {
     private Long id_user;
     private Long id_livre;
 
+    @Transient
     private Livre livre;
+    @Transient
     private User user;
 }
